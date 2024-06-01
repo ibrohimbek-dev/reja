f25b0fe9940422ca231a4837b4209e155213194f
Ibrohimbek-Dev <programming0127@gmail.com>
2024-06-01 22:57:06 +0900

    TASK F is started

---
+++ app.js
@@ -0,0 +1,98 @@
+// 2024-05-28:
+// Lesson 29th
+// Deployment
+
+// import main module & packages:
+const express = require("express");
+const app = express();
+
+// Call MongoDB
+const db = require("./server").db();
+const mongodb = require("mongodb");
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 3 session (views codes):
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 session (route codes):
+
+// get all items
+app.get("/", (req, res) => {
+  db.collection("plan")
+    .find()
+    .toArray((err, data) => {
+      if (err) {
+        console.log("Collection Error: ", err.message);
+        res.end("(app.get/) something went wrong");
+      } else {
+        console.log("Collection Data: ", data);
+        res.render("plan", { items: data });
+      }
+    });
+});
+
+// post an item
+app.post("/create-item", (req, res) => {
+  // STEP 2
+  const new_plan = req.body.plan;
+  console.log("new_plan: ", new_plan);
+  // STEP 3
+  db.collection("plan").insertOne({ plan: new_plan }, (err, data) => {
+    console.log("DATA.OPS APP.JS: ", data.ops);
+    res.json(data.ops[0]);
+  });
+});
+
+// delete an item
+app.post("/delete-item", (req, res) => {
+  const id = req.body.id;
+  console.log("deletion id: ", id);
+
+  db.collection("plan").deleteOne(
+    { _id: new mongodb.ObjectId(id) },
+    (err, data) => {
+      if (err) {
+        console.log("Error deleting item: ", err.message);
+        res
+          .status(500)
+          .json({ state: "error", message: "Failed to delete item" });
+      } else {
+        res.json({ state: "success" });
+      }
+    }
+  );
+});
+
+// edit an item
+app.post("/edit-item", (req, res) => {
+  const data = req.body;
+  console.log(data);
+
+  db.collection("plan").findOneAndUpdate(
+    {
+      _id: new mongodb.ObjectId(data.id),
+    },
+    { $set: { plan: data.new_input } },
+    (err, data) => {
+      res.json({ state: "success" });
+    }
+  );
+});
+
+// delete all item at once
+app.post("/delete-all", (req, res) => {
+  if (req.body.delete_all) {
+    db.collection("plan").deleteMany(() => {
+      res.json({ state: "All plans are deleted successfully" });
+    });
+  }
+});
+
+module.exports = app;
+
+// 29 - dars shu yerda yakunlandi
+// 수고 하셨습니다!
---
+++ train.js
@@ -0,0 +1,40 @@
+// 2024-05-28
+// MIT 14 TASK D
+
+// Ikkita parametra ega function tuzing, va functioning
+// berilgan birinchi va ikkinchi parametr qiymatlari o'zaro to'liq
+// mos kelsa true qiymat qaytarsin
+
+// Masalan: checkContent("mitgroup", "gmtiprou");
+// Yuqoridagi misolda birinchi va ikkinchi parametr qiymatli bir xil
+// ya'ni bir xil harflar qatnashganligi uchun true qiymat qaytaradi.
+
+// ----------------------------------------------------------------
+// console.log("----------------------------------------------------------------");
+// console.log("METHOD ONE:");
+
+// const checkContent_1 = (match, text) => {
+//   return text.split("").every((txt) => match.includes(txt));
+// };
+
+// const result_1 = checkContent_1("mitgroup", "gmtiprou");
+// console.log("Same text? => ", result_1);
+
+// // ----------------------------------------------------------------
+// console.log("----------------------------------------------------------------");
+// console.log("METHOD TWO:");
+
+// const checkContent_2 = (match, text) => {
+//   return text.split("").filter((txt) => !match.includes(txt)).length > 0
+//     ? false
+//     : true;
+// };
+
+// const result_2 = checkContent_2("mitgroup", "gmtiprouooooo");
+// console.log("Same text? => ", result_2);
+
+
+// ------------------------------------------------------------------------------------------------
+// TASK F is Started from Here
+// 2024-06-01:
+
